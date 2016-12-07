16a951fa8c1e1ee5f1a373c7af6bad7c808cbb2d
Maksim Khanickiy <maksim.kanickiy@ilogos.biz>
2016-12-07 14:29:44 +0200

    + movement controller

---
+++ Assets/Scripts/Tank/MovementController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour 
+{
+	[SerializeField]
+	private float _flatSpeed;
+	[SerializeField]
+	private float _turnSpeed;
+
+	private Transform _cachedTransform;
+
+	void Awake()
+	{
+		_cachedTransform = transform;
+	}
+
+	public void Move(float movementInput)
+	{
+		Vector3 movement = transform.forward * movementInput;// * _flatSpeed * Time.deltaTime;
+		//_cachedTransform.Translate (_cachedTransform.position + movement);
+		_cachedTransform.position = Vector3.MoveTowards(_cachedTransform.position, _cachedTransform.position + movement, _flatSpeed * Time.deltaTime);
+	}
+
+	public void Turn(float turnInput)
+	{
+		float turn = turnInput * _turnSpeed * Time.deltaTime;
+
+		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+		_cachedTransform.rotation = _cachedTransform.rotation * turnRotation;
+	}
+
+}
---
+++ Assets/Scripts/Tank/TankMainController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMainController : MonoBehaviour {
+	[SerializeField]
+	private MovementController _movementController;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float movementInput = Input.GetAxis ("Vertical");
+		float turnInput = Input.GetAxis ("Horizontal");
+
+		_movementController.Move (movementInput);
+		_movementController.Turn (turnInput);
+	
+	}
+}
