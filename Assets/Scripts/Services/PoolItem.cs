74c9bf2a3e62e235ce673081fdd3d08dae540a96
Maksim Khanickiy <maksim.kanickiy@ilogos.biz>
2016-12-07 13:34:19 +0200

    + object pool

---
+++ Assets/Scripts/Services/PoolItem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class PoolItem 
+{
+	public string Key;
+	public GameObject Value;
+	public int PoolSize;
+}
---
+++ Assets/Scripts/Services/SimplePool.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class SimplePool : MonoBehaviour {
+
+	[SerializeField]
+	private int _defaultSize;
+
+	[SerializeField]
+	private List<PoolItem> _items;
+
+	private static Dictionary<string, List<GameObject>> _pool;
+	private static Transform parent;
+
+	void Awake()
+	{
+		parent = transform;
+
+		if (_pool == null)
+			_pool = new Dictionary<string, List<GameObject>> ();
+
+		//fill pool
+		foreach (PoolItem item in _items) 
+		{
+			_pool [item.Key] = new List<GameObject> ();
+
+			for (int i = 0; i <= item.PoolSize - 1; i++) 
+			{
+				GameObject go = CreateItem (item.Value);
+				_pool [item.Key].Add (go);
+			}
+
+		}
+	}
+
+
+	public static GameObject GetPooledItem(string item)
+	{
+		List<GameObject> pooledObjects = null;
+
+		if (_pool.TryGetValue (item, out pooledObjects)) 
+		{
+			GameObject last = pooledObjects [pooledObjects.Count - 1];
+
+			if (!last.activeInHierarchy) 
+			{
+				pooledObjects.RemoveAt (pooledObjects.Count - 1);
+				pooledObjects.Insert (0, last);
+				return last;
+			} 
+			else 
+			{
+				pooledObjects.Add (CreateItem (last));
+				return pooledObjects [pooledObjects.Count - 1];
+			}
+		}
+
+		throw new Exception ("No pooled item with name : "+item);
+	}
+
+	private static GameObject CreateItem(GameObject prefab)
+	{
+		GameObject go =  Instantiate (prefab);
+		go.name = prefab.name;
+		go.transform.SetParent (parent);
+		go.SetActive (false);
+		return go;
+	}
+
+
+}
